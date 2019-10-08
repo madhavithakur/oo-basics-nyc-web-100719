52b319ea32716adde9cd8574fff026fc2436dc4f
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-08 20:20:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book 
   
-  def initialize(title, author_name, page_count, genre)
+  def initialize(title) #, author_name, page_count, genre)
     @title = title
     @author_name = author_name
     @page_count = page_count
